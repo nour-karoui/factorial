0891a9b64b3aff310fd7ea35e21774079fbe04dd
nour-karoui <nkaroui1998@gmail.com>
2021-11-12 10:52:20 +0100

    calculating factorial

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const factorial = require('factorial');
 const app = express();
 
 app.get('/:id', (req, res, next) => {
+    console.log('calculating factorial of', req.params.id);
     const factorialResult = factorial(parseInt(req.params.id));
     res.status(200).send(`factorial of ${req.params.id} is ${factorialResult}`);
 })
